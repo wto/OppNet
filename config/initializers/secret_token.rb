7ab9d6930afd516be67fb2972a5b580de0108367
Thomas Oppenheim <wtoppenheim@gmail.com>
2013-11-25 23:25:25 -0500

    I'm pimpin making mah social network.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OppNet::Application.config.secret_token = '01dee642e2564055cfa246442ddaf32c100dd2e4f5765f6eeb4b08e2ff7f59b315e4b97f04bc5c4333bd2dae5ddfd5d7cae0d309dc205cff139899c7d62dec2f'
